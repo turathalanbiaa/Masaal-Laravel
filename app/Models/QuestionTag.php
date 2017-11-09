23821fb3bb651bbc892fddfde94c6813460d5d95
Karrar <karrar@gmail.com>
2017-11-09 21:09:15 +0300

    made controllers

---
+++ app/Models/QuestionTag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionTag extends Model
+{
+    protected $table = "question_tag";
+    public $timestamps = false;
+}
