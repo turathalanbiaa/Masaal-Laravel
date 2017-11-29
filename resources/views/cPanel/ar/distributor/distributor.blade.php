a5eaf61cf621c2932968930c343ba6846d53b14c
= <=>
2017-11-29 09:00:32 +0300

    compelete distrbution question

--- resources/views/cPanel/ar/distributor/distributor.blade.php
+++ resources/views/cPanel/ar/distributor/distributor.blade.php
@@ -16,10 +16,13 @@
                 @if(count($questions) > 0)
                     @foreach($questions as $question)
                         <div class="ui teal segment">
+                            <div class="ui dimmer" id="dimmer">
+                                <div class="ui text loader">جاري التحميل...</div>
+                            </div>
                             <p class="ui green header">السؤال:-</p>
                             <p>{{$question->content}}</p>
                             <div class="ui divider"></div>
-                            <form class="ui form" method="post" action="/control-panel/{{$lang}}/distribution">
+                            <div class="ui form">
                                 {!! csrf_field() !!}
                                 <input type="hidden" name="questionId" value="{{$question->id}}">
                                 <div class="sixteen wide field">
@@ -35,16 +38,16 @@
                                     </div>
                                 </div>
                                 <div class="sixteen wide field">
-                                    <button class="ui green button" type="submit">ارسال</button>
-                                    <button class="ui left floated red button"  data-questionId="{{$question->id}}" data-action="convert-type-question">
+                                    <button class="ui green button" data-action="distribute-question">ارسال</button>
+                                    <button class="ui left floated red button" data-token="{!! csrf_token() !!}" data-question-id="{{$question->id}}" data-action="change-question-type">
                                         @if($question->type == \App\Enums\QuestionType::FEQHI)
-                                            {{"تحويل الى اسئلة العقائد"}}
+                                            {{"تحويل الى العقائد"}}
                                         @elseif($question->type == \App\Enums\QuestionType::AKAEDI)
-                                            {{"تحويل الى اسئلة الفقه"}}
+                                            {{"تحويل الى الفقه"}}
                                         @endif
                                     </button>
                                 </div>
-                            </form>
+                            </div>
                         </div>
                     @endforeach
 
@@ -55,7 +58,7 @@
                     <div class="ui massive info message">
                         <div class="ui hidden divider"></div>
                         <div class="ui hidden divider"></div>
-                        <div class="ui celled aligned header">لاتوجد اسئلة جديدة لعرضها</div>
+                        <div class="ui center aligned header">لاتوجد اسئلة جديدة لعرضها</div>
                         <div class="ui hidden divider"></div>
                         <div class="ui hidden divider"></div>
                     </div>
@@ -72,10 +75,76 @@
         $('.pagination').addClass('ui right aligned pagination menu');
         $('.pagination').css({'padding':'0','font-size':'15px'});
         $('.pagination').find('li').addClass('item');
-        $('.ui.form').form({
-            fields: {
-                respondentId: {rules: [{type   : 'empty'}]}
-            }
+
+        $("button[data-action='distribute-question']").click(function ()
+        {
+            var _token = $(this).parent().parent().find("input[type='hidden'][name='_token']").val();
+            var questionId = $(this).parent().parent().find("input[type='hidden'][name='questionId']").val();
+            var respondentId = $(this).parent().parent().find("input[type='hidden'][name='respondentId']").val();
+            $(this).parent().parent().parent().find('#dimmer').addClass("active");
+
+            $.ajax({
+                type: "POST",
+                url: '/control-panel/distribution',
+                data: {_token:_token, questionId:questionId, respondentId:respondentId},
+                datatype: 'json',
+                success: function(result) {
+                    if (result["question"] == "NotFound")
+                        snackbar("لايوجد مثل هذا السؤال." , 3000 , "warning");
+
+                    else if(result["respondent"] == "NotFound")
+                        snackbar("لم يتم اختيار المجيب." , 3000 , "warning");
+
+                    else if (result["success"] == false)
+                        snackbar("لم يتم تحويل السؤال الى المجيب !!، حاول مرة اخرى." , 3000 , "error");
+
+                    else if (result["success"] == true)
+                        snackbar("تم تحويل السؤال الى المجيب بنجاح." , 3000 , "success");
+                },
+                error: function() {
+                    snackbar("تحقق من الاتصال بالانترنت" , 3000 , "error");
+                } ,
+                complete : function() {
+                    $('#dimmer').removeClass("active");
+                }
+            });
+        });
+
+        $("button[data-action='change-question-type']").click(function ()
+        {
+            var questionId = $(this).data('question-id');
+            var _token = $(this).data('token');
+            var button = $(this);
+            button.addClass("loading");
+
+            $.ajax({
+                type: "POST",
+                url: '/control-panel/change-question-type',
+                data: {questionId:questionId, _token:_token},
+                datatype: 'json',
+                success: function(result) {
+                    if (result["question"] == "NotFound")
+                        snackbar("لايوجد مثل هذا السؤال." , 3000 , "warning");
+
+                    else if (result["success"] == false)
+                        snackbar("لم يتم تحويل نوع السؤال." , 3000 , "error");
+
+                    else if (result["success"] == true)
+                        snackbar("تم تجويل نوع السؤال." , 3000 , "success");
+                },
+                error: function() {
+                    snackbar("تحقق من الاتصال بالانترنت" , 3000 , "error");
+                } ,
+                complete : function() {
+                    button.removeClass("loading");
+                    var segment = button.parent().parent().parent();
+                    segment.addClass("scale");
+                    segment.transition({
+                        animation  : 'scale',
+                        duration   : '1s'
+                    });
+                }
+            });
         });
     </script>
 @endsection
