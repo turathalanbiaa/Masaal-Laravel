83c0bbc11c3f7de8832e22f4cd0e51b39bb4af8a
karrar <karrar@email.com>
2018-02-01 13:44:05 +0300

    translate to fr 3

--- resources/views/fr/question/send_question.blade.php
+++ resources/views/fr/question/send_question.blade.php
@@ -1,101 +1,99 @@
 @extends("fr.layout.main")
 
-
 @section("content")
     <div class="ui green segment">
 
-        <a class="ui right tag label" href="/fr/index/{{$question->type}}">
-            @if($question->type == 2)
-                Doctrinales
+        <form method="post" action="/fr/send" class="ui form">
+            {!! csrf_field() !!}
+            <div class="field">
+                <label>Tapez votre question
 
-                <a class="ui  teal tag label"
-                   href="/fr/search?type={{$question->type}}&id={{$question->categoryId}}">
-                    {{$question->category}}
-
-
-                </a>
-            @elseif($question->type == 1)
-
-                Jurispuridence
-
-
-                <a class="ui tag label"
-                   href="/fr/search?type={{$question->type}}&id={{$question->categoryId}}">
-                    {{$question->category}}
-
-
-                </a>
-            @endif
-
-        </a>
-
-
-        <a href="/fr/single-question/{{$question->id}}" class="ui right = label">Question:
-            \ {{$question->id}}</a>
-
-        <h3 class="ui header">
-            <img src="/img/man.jpg">
-            <div class="content ">
-                <div class="sub header">{{$question->userDisplayName}}</div>
-                <div class="sub header">{{$question->time}}</div>
+                    :</label>
+                <textarea rows="5" name="message"></textarea>
             </div>
+            <div class="ui centered error message"></div>
+            <div class="grouped fields">
+                <label> Confidentialité
 
+                    ؟</label>
+                <div class="field">
+                    <div class="ui radio checkbox">
+                        <input value="1" type="radio" name="privacy" checked="checked">
+                        <label> Privé
 
-        </h3>
-
-        <p>{{$question->content}}</p>
-        <div class="ui divider"></div>
-        <p>الجواب :</p>
-        <p>{{$question->answer}}</p>
-
-
-        <div class="ui hidden divider"></div>
-
-        @if($question->image !="")
-            <div class="ui  icon ">
-
-
-                <img class="ui right bordered large image"
-                     src="{{\App\Enums\ImagePath::path_answer . $question->image}}">
+                        </label>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="ui radio checkbox">
+                        <input value="2" type="radio" name="privacy">
+                        <label> Public
+                        </label>
+                    </div>
+                </div>
             </div>
-        @endif
+            <div class="ui form">
+                <div class="grouped fields">
+                    <label>Sélectionner une classification
+
+                        ؟</label>
+                    <div class="field">
+                        <div class="ui radio checkbox">
+                            <input value="1" type="radio" name="type" checked="checked">
+                            <label> Jurisprudence
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="ui radio checkbox">
+                            <input value="2" type="radio" name="type">
+                            <label>Doctrinales
 
 
-        <div class="ui hidden divider"></div>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="ui radio checkbox">
+                            <input value="1" type="radio" name="type">
+                            <label>pas de réponses
 
-
-        @if($question->videoLink !="")
-            <div>
-                <br>
-                <i class="video icon"></i>
-                <label>الفيديو : </label>
-                <a  href="{{$question->videoLink}}">                   اضغظ هنا لمشاهدة الفيديو</a>
-
-
-
-                {{--<div class="ui embed" data-url="{{$one_question->videoLink}}" data-placeholder="{{\App\Enums\ImagePath::path_post . "green.png"}}"></div>--}}
-                {{--<div class="ui embed" data-source="youtube" data-id="{{$one_question->videoLink}}" data-icon="play"--}}
-                {{--data-placeholder="{{\App\Enums\ImagePath::path_post . "green.png"}}"></div>--}}
-
-
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+            <button type="submit" class="ui green large button">Envoyer une question
 
-        <div class="ui hidden divider"></div>
+            </button>
 
-        @if($question->externalLink !="")
-            <div class="ui icon">
-
-                <i class="linkify icon"></i>
-                <label>المصدر : </label>
-                <a target="_blank" href="{{$question->externalLink}}">اضفط
-                    هنا لزيارة المصدر</a>
-            </div>
-        @endif
+        </form>
 
 
     </div>
+
+    <script>
+        $('.ui.checkbox').checkbox();
+        $('.ui.form')
+            .form({
+                fields: {
+                    message: {
+                        identifier: 'message',
+                        rules: [
+                            {
+                                type: 'minLength[6]',
+                                prompt: 'please enter your Question'
+                            }
+                        ]
+                    }
+
+                }
+            })
+        ;
+    </script>
+
     <script>
         $('.ui.embed').embed();
     </script>
+
 @endsection
