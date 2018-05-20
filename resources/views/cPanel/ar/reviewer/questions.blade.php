bd7ede546ddc55aabdb863b877b8de671e5a890b
karrar <karrar@email.com>
2018-05-20 15:38:49 +0300

    show name of admin to reviewer

--- resources/views/cPanel/ar/reviewer/questions.blade.php
+++ resources/views/cPanel/ar/reviewer/questions.blade.php
@@ -49,7 +49,7 @@
                                     <p>
                                         <span>المجيب</span>
                                         <i class="long arrow left icon" style="font-size: medium; font-weight: bold;"></i>
-                                        <span>{{$question->name}}</span>
+                                        <span>{{$question->Admin["name"]}}</span>
                                     </p>
 
                                     <p>
